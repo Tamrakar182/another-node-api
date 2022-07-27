a392903babcb73bd10018e653e96ef14003f5441
Manjul <manjultamrakar4@gmail.com>
2022-07-27 21:41:19 +0545

    added some basic error handling

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
+//handle incoming GET requests to /orders
 router.get('/', (req,res, next) => {
     res.status(200).json({
-        message:"Handling GET requests to /orders"
+        message:"Orders were fetched"
     })
 });
 
 router.post('/', (req,res, next) => {
     res.status(201).json({
-        message:"Handling POST requests to /orders"
+        message:"Order was created"
     })
 });
 
